c04e0e8b908abd493d6e08735a9031405e5fe051
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-07-14 14:12:40 -0700

    reload command uses docker-compose

--- reload.sh
+++ reload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker exec vs-code-server-proxy bash -c "nginx -T >/dev/null && /etc/init.d/nginx reload"
+docker-compose exec nginx bash -c "nginx -T >/dev/null && /etc/init.d/nginx reload"
 if [[ "$?" != "0" ]]; then
 	echo "ERROR reloading"
 	exit 1
