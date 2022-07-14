b273e85d05412a539beeeca1014864b1546609fb
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-07-14 16:10:24 -0700

    scripts sh instead of bash

--- reload.sh
+++ reload.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 docker-compose exec nginx bash -c "nginx -T >/dev/null && /etc/init.d/nginx reload"
-if [[ "$?" != "0" ]]; then
+if [ "$?" != "0" ]; then
 	echo "ERROR reloading"
 	exit 1
 else
