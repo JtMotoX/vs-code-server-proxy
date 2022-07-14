f6c8c233edbe918d17a4ce8d1b58d68637752166
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-07-14 14:25:17 -0700

    adding a run script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+touch .connection-token
+if [ "$(cat .connection-token)" = "" ]; then
+	echo "You need to set the password in the file: .connection-token"
+	exit 1
+fi
+
+docker-compose up -d
+
+/usr/local/bin/code-server serve-local --connection-token-file .connection-token
